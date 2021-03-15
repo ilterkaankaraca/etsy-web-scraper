bc13d8b0cdcd3d6645df000ca039711e7a361d53
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2021-03-15 21:56:10 +0100

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -5,6 +5,7 @@ import pandas
 
 excel = pandas.read_excel("2.xlsx")
 links=excel[excel.columns[0]]
+names=excel[excel.columns[1]]
 vgm_url = ''
 sales={}
 for i in range(len(links)):
@@ -15,10 +16,14 @@ for i in range(len(links)):
         soup = BeautifulSoup(html_text, 'html.parser')
         if soup.body.find('span', attrs={'class': 'wt-text-caption wt-no-wrap'}) is not None:
             sales[links[i]] = soup.body.find('span', attrs={'class': 'wt-text-caption wt-no-wrap'}).text
+
         else:
             sales[links[i]] = '-'
     else:
         sales['empty'+str(i)] = 'empty'+str(i)
 df = pandas.DataFrame(data=sales, index=[0])
-df.T.to_excel('sales.xlsx')
+print(names)
+df=df.T
+df['Name'] = names.to_list()
+df.to_excel('sales.xlsx')
 
