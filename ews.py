fa3e83ee2e22e5ed099e76a29292cc9418fda46b
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2021-03-21 12:49:00 +0100

    use and improve ews.py

--- ews.py
+++ ews.py
@@ -3,13 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 import pandas
 
-def get_sale_number(link : str):
-  html_text = requests.get(link).text
+def get_sales(link : str):
+  request = requests.get(link)
+  html_text = request.text
+  status = request.status_code
   soup = BeautifulSoup(html_text, 'html.parser')
   if soup.body.find('span', attrs={'class': 'wt-text-caption wt-no-wrap'}) is not None:
     sale_number = soup.body.find('span', attrs={'class': 'wt-text-caption wt-no-wrap'}).text[:-6].replace(',','')
-  else:
-    sale_number = None
+  elif soup.body.find('span', attrs={'class': 'wt-text-caption wt-no-wrap'}) is None or status[0]!='2':
+    sale_number = '-'
   return sale_number
-
-
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from numpy import empty
-import requests
-from bs4 import BeautifulSoup
 import pandas
-
+import ews
 excel = pandas.read_excel("2.xlsx")
 links=excel[excel.columns[0]]
 names=excel[excel.columns[1]]
@@ -11,14 +8,7 @@ sales={}
 for i in range(len(links)):
     if pandas.notna(links[i]):
         print(str(i) + '--' + str(links[i]))
-        vgm_url = str(links[i])
-        html_text = requests.get(vgm_url).text
-        soup = BeautifulSoup(html_text, 'html.parser')
-        if soup.body.find('span', attrs={'class': 'wt-text-caption wt-no-wrap'}) is not None:
-            sales[links[i]] = soup.body.find('span', attrs={'class': 'wt-text-caption wt-no-wrap'}).text[:-6].replace(',','')
-
-        else:
-            sales[links[i]] = '-'
+        sales[links[i]]= ews.get_sales(links[i])
     else:
         sales['empty'+str(i)] = 'empty'+str(i)
 df = pandas.DataFrame(data=sales, index=[0])
